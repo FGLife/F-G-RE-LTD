2719c78ff338b73e0264c0d9feb0c5c568255f9c
KatyClark <katherine.clark@fglife.com>
2017-11-13 13:45:45 -0500

    Updates

--- app/include/sendemail.php
+++ app/include/sendemail.php
@@ -24,7 +24,7 @@ if( isset( $_POST['template-contactform-submit'] ) AND $_POST['template-contactf
 
         $botcheck = $_POST['template-contactform-botcheck'];
 
-        $toemail = ''; // info@fgreltd.bm
+        $toemail = ''; // inquiries@fgreltd.bm
         $toname = ''; // Information F&G Re Ltd.
         if( $botcheck == '' ) {
 
